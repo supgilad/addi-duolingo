849ba0356e8efb31bfe1ec22d5b634b241a47f6d
gilad <gilad@gilad.com>
2020-11-19 20:17:30 +0200

    removed flask and removed user information  to env

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-import trace
-import traceback
-
+import os
 import duolingo
-import time
+
+password = os.environ['PASSWORD']
+user = os.environ['USER']
 
 
 def run_duolingo():
-    d = duolingo.Duolingo('addi390582', '132457')
+    d = duolingo.Duolingo(user, password)
     ret = d.buy_streak_freeze()
     return ret
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    try:
-        return run_duolingo()
-    except Exception as e:
-        print(e)
-        traceback.print_exc()
-        # raw_input("Press Enter to Quit..")
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
-
+    print(run_duolingo())
